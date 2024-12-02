9c4b692fb48e9de418f090f7dbcc024dff168c2f
Vtthuonggg <thuong23042003@gmail.com>
2024-12-02 15:34:00 +0700

    fix upload img

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -16,17 +16,7 @@ class ImageController extends Controller
      */
     public function upload(Request $request)
     {
-        // Định nghĩa các quy tắc xác thực
-        $validator = Validator::make($request->all(), [
-            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
-            'folder' => 'nullable|string', // Thư mục lưu trữ tùy chọn
-        ]);
-
-        // Kiểm tra xác thực
-        if ($validator->fails()) {
-            return response()->json($validator->errors(), 422);
-        }
-
+      
         // Xử lý upload hình ảnh
         if ($request->hasFile('image')) {
             $folder = $request->input('folder', 'images'); // Mặc định lưu trong thư mục 'images'
